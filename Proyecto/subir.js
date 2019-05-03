b0d7904ff6fc860092f43302732b88abf1c4f565
Victor.ITAM <victor.itam.mx@outlook.com>
2019-05-03 12:30:11 -0500

    subir propiedad, falta enlazar con login

---
+++ Proyecto/subir.js
@@ -0,0 +1,67 @@
+    /**
+
+     * Array con las imagenes y enlaces que se iran mostrando en la web
+
+     */
+
+    var imagenes=new Array(
+
+        ['imagenesSubirPropiedad/1.jpg'],
+
+        ['imagenesSubirPropiedad/2.jpg'],
+
+        ['imagenesSubirPropiedad/3.jpg'],
+
+        ['imagenesSubirPropiedad/4.jpg']
+
+    );
+
+ 
+
+    /**
+
+     * Funcion para cambiar la imagen y link
+
+     */
+
+    function rotarImagenes()
+
+    {
+
+        // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+
+        var index=Math.floor((Math.random()*imagenes.length));
+
+ 
+
+        // cambiamos la imagen
+
+        document.getElementById("imagen").src=imagenes[index][0];
+
+        
+
+    }
+
+ 
+
+    /**
+
+     * Función que se ejecuta una vez cargada la página
+
+     */
+
+    onload=function()
+
+    {
+
+        // Cargamos una imagen aleatoria
+
+        rotarImagenes();
+
+ 
+
+        // Indicamos que cada 5 segundos cambie la imagen
+
+        setInterval(rotarImagenes,5000);
+
+    }
